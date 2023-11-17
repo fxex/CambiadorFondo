337f63c484aa226e28ed40065c46d64a78a9e405
fxex <hugoantoniofrey2003@gmail.com>
2023-11-17 11:56:05 -0300

    cambie el nombre

--- ios/CambiadorFondoTests/CambiadorFondoTests.m
+++ ios/CambiadorFondoTests/CambiadorFondoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface pruebaTests : XCTestCase
+@interface CambiadorFondoTests : XCTestCase
 
 @end
 
-@implementation pruebaTests
+@implementation CambiadorFondoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
